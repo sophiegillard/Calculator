b3e16cf8ff67b9443f97bc2ea9f2567027bc0f4a
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-08-03 16:33:29 +0200

    creation of the history. No solution to delete the history yet. No solution to make a new calcul without pushin AC

--- calculator.js
+++ calculator.js
@@ -8,15 +8,33 @@ calculatorDiv.appendChild(calculatorBoard);
 console.log(calculatorDiv)
 
 //CALCULATOR SCREEN
+    //DIV for the numbers
+var calculDiv= document.createElement('div');
+calculDiv.className='calculDiv';
+calculatorScreen.appendChild(calculDiv);
+calculDiv.setAttribute('type', 'text');
+
+    //create span for the history
 var textView= document.createElement('span');
 textView.className='textView';
-calculatorScreen.appendChild(textView);
+calculDiv.appendChild(textView);
 textView.setAttribute('type', 'text');
 
+    //create span for the history
 var textResult= document.createElement('span');
 textResult.className='textResult';
-calculatorScreen.appendChild(textResult);
+calculDiv.appendChild(textResult);
 textResult.setAttribute('type', 'text');
+
+    //create span for the history
+var textHistory= document.createElement('span');
+textHistory.className='textHistory';
+calculatorScreen.appendChild(textHistory);
+textHistory.setAttribute('type', 'text');
+       //create LIST for the history
+       var listHistory= document.createElement('ul');
+        listHistory.className='textHistoryUl';
+        textHistory.appendChild(listHistory);
 
 //CALCULOTAR BOARD
 //Function to create the operators
@@ -71,7 +89,8 @@ numbersAndBasicOperators.className='numbersAndBasicOp';
 
 //END OF CREATING THE HTML
 
-//BEGINNING 
+
+//BEGINNING of the functional aspect
 
 //Display the numbers and operators on the screen
 let buttonsCalculator = document.querySelectorAll("input[type=button]");
@@ -81,13 +100,25 @@ console.log(buttonsLength);
 let calculScreen =  document.querySelector(".textView");
 let resultScreen =  document.querySelector(".textResult");
 
+// adding the event CLick to all buttons
 for (let i = 0; i < buttonsLength; i++) {
     buttonsCalculator[i].addEventListener("click", calcul)
     }
 
+// function to calculate the strings
 function computeResult(str){
     return Function('return ' + str)()
     }
+
+ //Function to display to history of the operations in the calculator
+function history(num){
+    var listItem = document.createElement('li');
+    listItem.className = "historyItem";
+    listHistory.appendChild(listItem);
+    const histText = document.createTextNode(num + " = " + computeResult(num));
+    listItem.appendChild(histText);
+    console.log(listItem);
+}
     
 function calcul (event) {
     const value= event.target.value;
@@ -102,9 +133,13 @@ function calcul (event) {
             console.log("= was clicked");
             let num = textView.innerHTML;
             console.log(num);
-            console.log(computeResult(num));
+            history(num);
             resultScreen.innerHTML = "=" + computeResult(num);
+            var total = "=" + computeResult(num);
             break;
+        case "%":
+            calculScreen.innerHTML = '('+calculScreen.innerHTML+'/100'+')'+'*'; 
+        break;
         default:
         console.log("%s was clicked", value);
         calculScreen.innerHTML += value;
@@ -116,8 +151,17 @@ function calcul (event) {
 
 document.body.addEventListener("keyup", deleteLastDigit =>{
     if (deleteLastDigit.code ==='Backspace'){
-        console.log('it works');
+        let deletedigit = calculScreen.innerHTML;
+        calculScreen.innerHTML = deletedigit.slice (0, -1);
     }
 })
 
+// PRESSING NUMBERS TO DISPLAY DIGITS
+// for (let i = 0; i < buttonsLength; i++) {
+//     document.body.addEventListener("keyup", pressKeys =>{
+//         if (pressKeys.key ===[i]){
+//             calcul()        }
+//     })
+// }
 
+// PRESSING ENTER TO ACT LIKE EQUAL
